76ec7ef55a5431fdccf60cab9e87357fb4245a96
Golden Ekpendu <ekpendugolden1@gmail.com>
2023-01-08 18:38:18 +0100

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../images/logo.png'
+
+export const Navbar = () => {
+    return <nav>
+        <div>
+            <img src={logo} className=" w-[72.5px] h-[16px]" />
+        </div>
+    </nav>
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter': ["Inter", "sans-serif"],
+      },
+      colors: {
+        'background': '#FFFFFF',
+        'darkBackground': '#000000',
+        'textGreen': '#4AB9A5',
+        'textGray': '#545353',
+        'inputWhite': '#F9F9F9',
+      },
+  },
+  plugins: [],
+}
+}
